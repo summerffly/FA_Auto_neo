aa1ff692e95b26ff3da7aacf524ec23232527405
summerffly <afeism@foxmail.com>
2018-07-09 14:54:06 +0800

    FA_Auto_X_V2.11.7
    - 增加cp2GitHub.sh脚本

---
+++ Linux_Cmd/cp2GitHub.sh
@@ -0,0 +1,8 @@
+
+cp ./FA_Auto_X.cpp ./../../GitHub/FA_Auto_X/FA_Auto_X.cpp
+
+cp ./FA_Layer/FAitfX.h ./../../GitHub/FA_Auto_X/FA_Layer/FAitfX.h
+cp ./FA_Layer/FAitfX.cpp ./../../GitHub/FA_Auto_X/FA_Layer/FAitfX.cpp
+
+cp ./AS_Layer/ASitfX.h ./../../GitHub/FA_Auto_X/AS_Layer/ASitfX.h
+cp ./AS_Layer/ASitfX.cpp ./../../GitHub/FA_Auto_X/AS_Layer/ASitfX.cpp
